0c971e661a2838e56542f3141637bc9f33e11a42
Albert Shu <axs689@yahoo.com>
2014-02-02 16:00:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoogleHome::Application.config.secret_key_base = 'ed3a7e7a368a89ba2b6c3b53d646fa3ded935ace251168f5792e9a3fa625c5e0b1df880e9a83fad4902f7f7fc79a47e108c9a8c1f4ae03ef0f64ec60a67515fe'
